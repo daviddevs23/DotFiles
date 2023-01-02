757537940bf30c8429118a6151db0c1e41c5e2a5
David Hochstetler <daviddevs23@gmail.com>
2023-01-02 18:55:09 -0500

    quickly backing up2

--- system/nvim/install.sh
+++ system/nvim/install.sh
@@ -2,3 +2,4 @@
 
 yay -S neovim node npm
 yay -S clang llvm llvm-libs python3 python-pip pyright jedi-language-server rust cargo go gopls bash-language-server pandoc typescript-language-server
+sudo npm install -g svelte-language-server
