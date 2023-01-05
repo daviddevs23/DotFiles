826d4e854241659cda7ba112749f0ae6d1a44f7a
David Hochstetler <daviddevs23@gmail.com>
2023-01-05 09:47:03 -0500

    ready for reinstall

--- install.sh
+++ install.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+# Install yay
+sudo pacman -S git
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd $HOME
 
 yay -S udisks2 xdg-desktop-portal xdg-desktop-portal-wlr grim qt6-wayland qt5-wayland
 sudo systemctl enable udisks2
+
+yay -S easyeffects nfs-utils
+
+# etc/fstab additions
+# 10.18.39.3:/export/Mass /Mass  nfs      defaults    0       0
+# 10.18.39.3:/export/backups /backups  nfs      defaults    0       0
+
+# Notifications in sway
+# https://github.com/ErikReider/SwayNotificationCenter
