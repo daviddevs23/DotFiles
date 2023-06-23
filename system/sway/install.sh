a5730c78460e9582cfb88c8169cc667d5242b42d
David Hochstetler <daviddevs23@gmail.com>
2023-06-23 13:18:10 -0400

    Cleared out sway and added swaylock-effects

--- system/sway/install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-yay -S swaybg
-
-# To get virtual cameral working, use sudo modprobe v4l2loopback
--- system/swaylock/install.sh
+++ system/swaylock/install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 yay -S swaylock-effects
+ln -s $HOME/DotFiles/system/swaylock/swaylock $HOME/.config/
